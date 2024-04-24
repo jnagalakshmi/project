c63167de8e03d6d12367225982688b998f9bdcde
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-24 14:45:16 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -4,9 +4,9 @@
 
 
 
-// $con = mysqli_init();
-// mysqli_ssl_set($con,NULL,NULL, "{path to CA cert}", NULL, NULL);
-// mysqli_real_connect($conn, "agroserver.mysql.database.azure.com", "bhumi", "Agriculture1234", "bhumi", 3306, MYSQLI_CLIENT_SSL);
+ $con = mysqli_init();
+ mysqli_ssl_set($con,NULL,NULL, "new", NULL, NULL);
+ mysqli_real_connect($conn, "agroserver.mysql.database.azure.com", "bhumi", "Agriculture1234", "bhumi", 3306, MYSQLI_CLIENT_SSL);
 
 
 $mysqli = new mysqli('agroserver.mysql.database.azure.com', 'bhumi', 'Agriculture1234', 'bhumi');
