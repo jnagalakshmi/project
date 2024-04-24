fa96e3a53a7f4bccc7b39bc8cd9ad6314fde8871
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-24 17:44:55 +0530

    Update admin_users.php

--- admin_users.php
+++ admin_users.php
@@ -1,17 +1,5 @@
 <?php
-$servername = "localhost"; // Replace with your server name
-$username = "root"; // Replace with your username
-$password = ""; // Replace with your password
-$dbname = "bhumi"; // Replace with your database name
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
+include 'connection.php';
 $sql = "SELECT * FROM details"; // Replace 'student' with your actual table name
 $result = mysqli_query($conn, $sql);
 echo ' <span style="font-family: verdana, geneva, sans-serif;"><!DOCTYPE html>
