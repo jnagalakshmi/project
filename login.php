5a8d346fd8e0e442485a4d36517caddc8a545995
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-25 19:08:18 +0530

    Update login.php

--- login.php
+++ login.php
@@ -22,7 +22,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     // If user exists, set session and redirect to home page
     if($count == 1) {
         $_SESSION['username']=$username;
-        header("Location:agri-e-commerce-bhumi.azurewebsites.net/Home Page.php");
+        header("Location:agri-e-commerce-bhumi.azurewebsites.net");
      //echo "login successful";
      
         exit(); // It's good practice to exit after redirection to prevent further execution
