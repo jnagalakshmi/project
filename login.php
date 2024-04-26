891d907ffea22fc588f2009b103cebc45480342d
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-26 12:36:58 +0530

    Update login.php

--- login.php
+++ login.php
@@ -23,7 +23,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
      if($count == 1) {
       // echo"Welcome";
       $_SESSION['username']=$username;
-      header("Location: ../project/Home Page.php");
+      header("Location: ../project/HomePage.php");
    } else {
         echo "Your Login Name or Password is invalid";
     }
