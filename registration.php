8c535f415e20ebbb5e58be994d5588d3258804b2
jnagalakshmi <137597675+jnagalakshmi@users.noreply.github.com>
2024-04-30 10:08:36 +0530

    Update registration.php

--- registration.php
+++ registration.php
@@ -23,7 +23,7 @@ $email = $_POST['email'];
  
 if ($stmt->execute()) {
     // Redirect to a success page
-    header("Location: agri-e-commerce-bhumi.azurewebsites.net/login.html");
+    header("Location: login.html");
     exit;
 } else {
     echo "Registration failed. Error: " . $stmt->error;
